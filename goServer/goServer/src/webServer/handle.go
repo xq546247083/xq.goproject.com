c487ed5c5c2d96973629d501f75168c74a8a4b8f
肖强 <xiaoqiang@moyihuyu.com>
2017-10-18 11:34:14 +0800

    优化了代码

--- goServer/goServer/src/webServer/handle.go
+++ goServer/goServer/src/webServer/handle.go
@@ -49,7 +49,7 @@ func (handleObj *handle) ServeHTTP(responseWriter http.ResponseWriter, request *
 				if responseObj.Status == webServerObject.Success {
 					logTool.LogDebug(fmt.Sprintf("web服务器接受请求,请求地址：%s %s请求数据：%s %s返回数据：%s", requestObject.HTTPRequest.RequestURI, stringTool.GetNewLine(), valStr, stringTool.GetNewLine(), string(data)))
 				} else {
-					logTool.LogError(fmt.Sprintf("web服务器接受请求,请求地址：%s %s请求数据：%s %s返回数据：%s", requestObject.HTTPRequest.RequestURI, stringTool.GetNewLine(), valStr, stringTool.GetNewLine(), string(data)))
+					logTool.LogDebug(fmt.Sprintf("web服务器接受请求,请求地址：%s %s请求数据：%s %s返回数据：%s", requestObject.HTTPRequest.RequestURI, stringTool.GetNewLine(), valStr, stringTool.GetNewLine(), string(data)))
 				}
 			}
 		}
