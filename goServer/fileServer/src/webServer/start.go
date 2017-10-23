1b3ffcf0ca61b315ad21888bdc9ac021dde67dde
肖强 <xiaoqiang@moyihuyu.com>
2017-10-23 17:58:51 +0800

    添加监听文件获取路径

--- goServer/fileServer/src/webServer/start.go
+++ goServer/fileServer/src/webServer/start.go
@@ -1,10 +1,11 @@
 package webServer
 
 import (
-	"xq.goproject.com/commonTools/logTool"
 	"fmt"
 	"net/http"
 	"sync"
+
+	"xq.goproject.com/commonTools/logTool"
 )
 
 //StartServer 开启服务
@@ -15,9 +16,16 @@ func StartServer(wg *sync.WaitGroup, serverAddress string) {
 
 	//开启服务
 	logTool.Log(logTool.Info, fmt.Sprintf("Web服务器监听：%s", serverAddress))
-	fmt.Println( fmt.Sprintf("Web服务器监听：%s", serverAddress))
-	
-	if err := http.ListenAndServe(serverAddress, new(handle)); err != nil {
+	fmt.Println(fmt.Sprintf("Web服务器监听：%s", serverAddress))
+
+	//服务获取文件
+	http.HandleFunc("/upload/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+	//处理其他消息
+	http.Handle("/", new(handle))
+
+	if err := http.ListenAndServe(serverAddress, nil); err != nil {
 		logTool.LogObject(logTool.Error, err)
 	}
 }
