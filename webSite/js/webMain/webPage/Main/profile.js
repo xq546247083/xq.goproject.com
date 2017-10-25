dd25057fda78911e1ae36d3e0b6e95ba48a9d75d
肖强 <xiaoqiang@moyihuyu.com>
2017-10-25 17:01:55 +0800

    添加头像上传按钮

--- webSite/js/webMain/webPage/Main/profile.js
+++ webSite/js/webMain/webPage/Main/profile.js
@@ -20,6 +20,83 @@ $(document).ready(function() {
     $("#info").prepend("<p>邮箱：" + email + "</p>");
     $("#info").prepend("<p>性别:" + (sex ? "男" : "女") + "</p>");
     $("#info").prepend("<h2 class=\"media-heading\">" + fullName + "</h2><br/>");
+});
 
+//上传代码
+$(document).ready(function() {
+    var uploader = WebUploader.create({
+        pick: {
+            id: "#filePicker",
+            label: "点击上传头像",
+            multiple: false,
+        },
+        auto: true,
+        accept: {
+            title: "Images",
+            extensions: "gif,jpg,jpeg,bmp,png",
+            mimeTypes: "image/jpg,image/jpeg,image/png"
+        },
+        // compress: false,
+        compress: {
+            width: 600,
+            height: 400,
 
+            // 图片质量，只有type为`image/jpeg`的时候才有效。
+            quality: 90,
+
+            // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
+            allowMagnify: false,
+
+            // 是否允许裁剪。
+            crop: false,
+
+            // 是否保留头部meta信息。
+            preserveHeaders: true,
+
+            // 如果发现压缩后文件大小比原来还大，则使用原来图片
+            // 此属性可能会影响图片自动纠正功能
+            noCompressIfLarger: false,
+
+            // 单位字节，如果图片大小小于此值，不会采用压缩。
+            compressSize: 0
+        },
+        fileNumLimit: 1,
+        fileSizeLimit: 5242880,
+        fileSingleSizeLimit: 5242880,
+        chunked: true, //开启分片上传  
+        chunkSize: 1024 * 100, // 如果要分片，分多大一片？默认大小为5M  
+        chunkRetry: 3, //如果某个分片由于网络问题出错，允许自动重传多少次  
+        threads: 1, //上传并发数。允许同时最大上传进程数[默认值：3]  
+        duplicate: false, //是否重复上传（同时选择多个一样的文件），true可以重复上传  
+        prepareNextFile: true, //上传当前分片时预处理下一分片  
+        //-------------------------设置上传的服务器地址----------------------
+        server: WebMain.FileServerConfig + "APIFromFile/UploadPhoto",
+        formData: {
+            UserName: "",
+            Token: "",
+            UploadTime: "",
+            PicName: "",
+        },
+    });
+
+    uploader.on("click", function() {
+        return true;
+    });
+
+    uploader.on('uploadBeforeSend', function(obj, data, headers) {
+        //赋值
+        data.UploadTime = new Date().getTime();
+        data.PicName = "Album";
+        data.UserName = $.cookie("UserName");
+        data.Token = $.cookie("Token");
+    });
+
+    uploader.on('uploadError', function(e) {
+        toastr.error("提示", e);
+    });
+
+    uploader.on('uploadComplete', function(file) {
+        toastr.success("提示", "上传完成");
+        $("#filePicker").hide();
+    });
 });
