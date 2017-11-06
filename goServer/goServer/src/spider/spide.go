3a97b8e5ca0d9111afa8fb8de6732b30556c9cda
肖强 <xiaoqiang@moyihuyu.com>
2017-11-06 17:00:24 +0800

    优化了代码

--- goServer/goServer/src/spider/spide.go
+++ goServer/goServer/src/spider/spide.go
@@ -6,13 +6,13 @@ import "time"
 func Start() {
 	//  每分钟执行一次
 	for {
-		remainTime := time.Duration(60 - time.Now().Second())
-		time.Sleep(remainTime * time.Second)
-
 		// 获取当前时间的日期
 		now := time.Now()
-		if (now.Hour() == 5 || now.Hour() == 11 || now.Hour() == 17 || now.Hour() == 10) && now.Minute() == 0 {
+		if (now.Hour() == 5 || now.Hour() == 11 || now.Hour() == 17 || now.Hour() == 22) && now.Minute() == 0 {
 			go startZheTianSpider()
 		}
+
+		remainTime := time.Duration(60 - time.Now().Second())
+		time.Sleep(remainTime * time.Second)
 	}
 }
