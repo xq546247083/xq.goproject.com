ec537622d467411c60bbea6840d98240b9200874
xiaoqiang <546247083@qq.com>
2018-03-14 11:33:12 +0800

    通道优化

--- test/timer/main.go
+++ test/timer/main.go
@@ -12,5 +12,17 @@ func main() {
     ss:=<-timer.C
     fmt.Println(time.Now())
     fmt.Println(ss)
-    
+
+    timeOutChan()
+}
+
+// 超时通道
+func timeOutChan(){
+    var chanInt chan int
+    select{
+        case e:=<-chanInt:
+            fmt.Println(e)
+        case <-time.After(2*time.Second):
+            fmt.Println("timeout")
+    }
 }
