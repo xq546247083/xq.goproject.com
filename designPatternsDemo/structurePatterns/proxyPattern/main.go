7fa371dcf75f3b5f5552e507319fef7081a3931f
xiaoqiang <546247083@qq.com>
2018-04-20 18:09:02 +0800

    代理模式

--- designPatternsDemo/structurePatterns/proxyPattern/main.go
+++ designPatternsDemo/structurePatterns/proxyPattern/main.go
@@ -10,14 +10,20 @@ func main(){
 	proxyImageObj.display()
 }
 
+// ----------------图片----------------
 //  图片
 type image interface{
 	display()
+	loadFromDisk()
 }
 
 // 真正的图片
 type realImgae struct{
 	name string 
+}
+
+func (this *realImgae)loadFromDisk(){
+	fmt.Println("加载:",this.name);
 }
 
 func (this *realImgae)display(){
@@ -32,10 +38,13 @@ type proxyImage struct{
 	name string 
 }
 
+// 代理模式组合了从硬盘加载，且第二次显示的时候，不需要再加载
+// 而且代理图片少了一个加载的方法，控制了方法的数量
 func (this *proxyImage)display(){
 	if this.realImgaeObj==nil{
 		this.realImgaeObj=new(realImgae)
 		this.realImgaeObj.name=this.name
+		this.realImgaeObj.loadFromDisk()
 	}
 	
 	this.realImgaeObj.display()
