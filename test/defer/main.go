2164739b0322eef2af53e3d74cb1ad6f970fb4e3
xiaoqiang <546247083@qq.com>
2018-05-02 11:03:53 +0800

    添加了测试代码

---
+++ test/defer/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    defer_call()
+}
+
+// defer 是后进先出。
+// panic 需要等defer 结束后才会向上传递。 出现panic恐慌时候，会先按照defer的后入先出的顺序执行，最后才会执行panic。
+func defer_call() {
+    defer func() { fmt.Println("打印前") }()
+    defer func() { fmt.Println("打印中") }()
+    defer func() { fmt.Println("打印后") }()
+
+    panic("触发异常")
+}
