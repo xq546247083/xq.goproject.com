6f65a193b3b996641f99c7d5c2cc5d547af7cd9d
xiaoqiang <546247083@qq.com>
2018-05-12 16:25:37 +0800

    添加gops代码

---
+++ test/gops/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/google/gops/agent"
+)
+
+func main() {
+	if err := agent.Listen(agent.Options{
+		ShutdownCleanup: true, // automatically closes on os.Interrupt
+		ConfigDir:"Log",
+		Addr:":10018",
+	}); err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Hour)
+}
