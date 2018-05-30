d2de9f9dd0eaa1c608eb58f1c9b79b20d501d260
xiaoqiang <546247083@qq.com>
2018-05-30 14:25:06 +0800

    切片添加

--- test/slice/main.go
+++ test/slice/main.go
@@ -6,12 +6,27 @@ import (
 
 // 切片测试
 func main() {
-	a := []int{}
+	a := [3]int{1}
 	a1 := make([]int, 0)
 	a2 := make([]int, 0, 0)
 
-	a = append(a, 1)
 	a1 = append(a1, 1)
 	a2 = append(a2, 1)
 	fmt.Println(a, a1, a2)
+
+	TestSet(a)
+	fmt.Println(a, a1, a2)
+
+	TestSet2(&a)
+	fmt.Println(a, a1, a2)
+}
+
+// 传递数组
+func TestSet(intArray [3]int) {
+	intArray[0] = 2
+}
+
+// 传递数组的地址
+func TestSet2(intArray *[3]int) {
+	(*intArray)[0] = 3
 }
