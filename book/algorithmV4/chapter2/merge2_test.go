c519f1d99e24eec502e8c17f6e3d593f4f320a97
肖强 <546247083@qq.com>
2018-11-19 16:07:59 +0800

    分离归并排序文件

---
+++ book/algorithmV4/chapter2/merge2_test.go
@@ -0,0 +1,50 @@
+// 归并两个有序的数组，为一个有序数组。
+// 归并排序。
+package chapter1
+
+import (
+	"testing"
+)
+
+// TestMergelSort2 测试
+func TestMergelSort2(t *testing.T) {
+	nums := getRandomList(10000000)
+	t.Errorf("%v", mergeSort2(nums, 0, len(nums)-1))
+}
+
+// 优化的归并排序，在数据量大时，使用归并排序，其他时候，使用插入排序
+func mergeSort2(nums []int, min, max int) []int {
+	if min >= max {
+		return nums
+	}
+
+	mid := min + (max-min)/2
+	if max-min > 20 {
+		// 递归排序左半边
+		mergeSort(nums, min, mid)
+		// 递归排序右半边
+		mergeSort(nums, mid+1, max)
+		// 合并左右的排序数据
+		merge(nums, min, mid, max)
+	} else {
+		// 插入排序左半边
+		insertSort2(nums, min, mid)
+		// 插入排序右半边
+		insertSort2(nums, mid+1, max)
+		// 合并左右的排序数据
+		merge(nums, min, mid, max)
+	}
+
+	return nums
+}
+
+// 插入排序
+func insertSort2(nums []int, min, max int) []int {
+	for i := min; i <= max; i++ {
+		for j := i; j > min && less(nums[j], nums[j-1]); j-- {
+			nums = exch(nums, j, j-1)
+		}
+	}
+
+	return nums
+}
