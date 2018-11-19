4351b5bb03fff281a3b7e80c8373e483df4053aa
肖强 <546247083@qq.com>
2018-11-19 16:21:46 +0800

    添加注释

--- book/algorithmV4/chapter2/merge3_test.go
+++ book/algorithmV4/chapter2/merge3_test.go
@@ -16,6 +16,7 @@ func TestMergelSort3(t *testing.T) {
 func mergeSort3(nums []int) []int {
 	lenNums := len(nums)
 
+	// 步长 1 2 4 8...向上合并数组
 	for step := 1; step < lenNums; step = step + step {
 		for j := 0; j < lenNums-step; j = j + step + step {
 			merge(nums, j, j+step-1, min(lenNums-1, j+step+step-1))
